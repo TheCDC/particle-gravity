569cd5cccbb22c4ce27a99e1fed273af82be9faf
christopher <christoipher.chen@gmail.com>
2017-02-02 13:17:33 -0800

    initial commit

---
+++ graphical.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+import newtonian
+import pygame
+import random
+import time
+import sys
+import math
+import os
+
+
+def randcolor():
+    return tuple([int(random.random() * 255) for i in range(3)])
+
+
+class Turtle:
+
+    def __init__(self, surf, color):
+        self.surf = surf
+        self.pos = (0, 0)
+        self.pen = True
+        self.color = color
+
+    def setpos(self, pos):
+        if self.pen:
+            pygame.draw.aaline(self.surf, self.color, self.pos, pos, 1)
+        self.pos = pos[:]
+
+    def getpos(self):
+        return self.pos[:]
+
+    def penup(self):
+        self.pen = False
+
+    def pendown(self):
+        self.pen = True
+
+
+class GraphicalSimulation:
+
+    def __init__(self, surface, N=None):
+        self.surf = surface
+        self.bottom_layer = pygame.Surface(
+            (self.surf.get_width(), self.surf.get_height()))
+        self.size = (self.bottom_layer.get_width(),
+                     self.bottom_layer.get_height())
+        if not (N == None):
+            n = 12
+        else:
+            n = N
+
+        mass_range_exponents = (8, 12)
+        self.particles = [newtonian.Particle(random.randint(*[10**i for i in mass_range_exponents]),
+                                             [random.randint(0, a)
+                                              for a in self.size],
+                                             velocity=[(random.random() - 0.5) * 24 for i in range(2)]) for i in range(n)]
+        self.field = newtonian.ParticleField(self.particles)
+        self.turtles = [Turtle(self.bottom_layer, randcolor())
+                        for _ in self.particles]
+        self.top_layer = pygame.Surface(
+            (self.bottom_layer.get_width(), self.bottom_layer.get_height()))
+        for t, p in zip(self.turtles, self.field.get_particles()):
+            t.penup()
+            t.setpos(p.getpos())
+            t.pendown()
+        self.frames = 0
+
+    def update(self, step=1 / 100):
+        self.field.time_step(step)
+
+    def draw(self):
+        if self.frames == 0:
+            for t, p in zip(self.turtles, self.field.get_particles()):
+                t.setpos(p.getpos())
+                pygame.draw.circle(self.bottom_layer, t.color,
+                                   tuple(map(int, t.getpos())), int((math.log(p.mass**2))**(1 / 2)))
+        self.frames += 1
+        self.top_layer.fill((0, 0, 0))
+        self.surf.fill((0, 0, 0))
+        for t, p in zip(self.turtles, self.field.get_particles()):
+            t.setpos(p.getpos())
+            pygame.draw.circle(self.top_layer, t.color,
+                               tuple(map(int, t.getpos())), int((math.log(p.mass**2))**(1 / 2)))
+        self.surf.blit(self.bottom_layer, (0, 0),
+                       special_flags=pygame.BLEND_MAX)
+        self.surf.blit(self.top_layer, (0, 0), special_flags=pygame.BLEND_MAX)
+
+
+def main():
+    pygame.init()
+    CLOCK = pygame.time.Clock()
+    infoObj = pygame.display.Info()
+    WIDTH = int(infoObj.current_w)
+    HEIGHT = int(infoObj.current_h)
+    # WIDTH = 600
+    # HEIGHT = 600
+    size = (WIDTH, HEIGHT)
+    trail_surface = pygame.Surface(size)
+    DISPLAYSURF = pygame.display.set_mode(
+        size, pygame.DOUBLEBUF | pygame.FULLSCREEN)
+    sim = GraphicalSimulation(trail_surface, random.randint(3, 30))
+    ti = time.time()
+
+    while True:
+        tf = time.time()
+        if tf - ti >= 60:
+            pygame.image.save(DISPLAYSURF, os.path.join(
+                "screenshots", time.strftime("%Y-%m-%d %H-%M-%S") + ".png"))
+            for i in range(0,255,255//60):
+                DISPLAYSURF.fill((i,i,i))
+                pygame.display.update()
+                CLOCK.tick(60)
+            for i in range(255,0,-255//60):
+                DISPLAYSURF.fill((i,i,i))
+                pygame.display.update()
+                CLOCK.tick(60)
+            DISPLAYSURF.fill((0, 0, 0))
+            del sim
+            sim = GraphicalSimulation(DISPLAYSURF, random.randint(3, 12))
+            ti = time.time()
+
+        for event in pygame.event.get():
+            #~ print(event)
+            if event.type == pygame.QUIT or pygame.mouse.get_pressed()[0] == 1:
+                pygame.mixer.quit()
+                pygame.quit()
+                sys.exit()
+
+        granularity = 10
+        step = 0.5
+        for _ in range(granularity):
+            sim.update(step / granularity)
+        sim.draw()
+        CLOCK.tick(60)
+        # time.sleep(1/60)
+        DISPLAYSURF.blit(sim.surf, (0, 0))
+        pygame.display.update()
+        # print("yes")
+if __name__ == '__main__':
+    main()
---
+++ newtonian.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python3
+import turtle
+import random
+import math
+import numpy
+import time
+# G = 2.071e-43
+G = 6.67300e-11
+
+
+def distance(p1, p2):
+    return sum((p1.getpos() - p2.getpos())**2)**(1 / 2)
+
+
+def angle(p1, p2):
+    return math.atan2(*((p2.getpos() - p1.getpos())[2::-1]))
+
+
+def accel_vector(p1, p2):
+    F = G * p1.mass * p2.mass / distance(p1, p2)
+    a = F / p1.mass
+    theta = angle(p1, p2)
+    return numpy.array([math.cos(theta), math.sin(theta)]) * a
+
+
+class Particle:
+
+    def __init__(self, mass: float, position: numpy.array, velocity=None, anchored=False):
+        self.mass = mass
+        self.pos = numpy.array(position, dtype=float)
+        if velocity:
+            self.velocity = velocity
+        else:
+            self.velocity = numpy.zeros(len(position), dtype=float)
+        self.acceleration = numpy.array(self.velocity, dtype=float)
+        self.anchored = anchored
+
+    @property
+    def velocity(self):
+        return self._velocity
+
+    @velocity.setter
+    def velocity(self, np_array: numpy.array):
+        self._velocity = numpy.array(np_array)
+
+    def __add__(self, other):
+        if not isinstance(other, Particle):
+            raise ValueError(
+                "Can only add Particle to Particle not {}".format(repr(other)))
+        else:
+            if len(self.pos) != len(other.pos):
+                raise ValueError(
+                    "Can not add particles with different dimensionalities.")
+            ms = numpy.array([self.mass, other.mass])
+            sm = sum(ms)
+            p = Particle(self.mass + other.mass,
+                         (self.pos * self.mass / sm + other.pos * other.mass / sm) / 2)
+            return p
+
+    def getpos(self):
+        return numpy.array(self.pos)
+
+    def be_pulled(self, other):
+        self.acceleration = accel_vector(self, other)
+        # print(self.acceleration)
+
+    def add_pull(self, other):
+        self.acceleration += accel_vector(self, other)
+
+    def simulate(self, time_step):
+        if not self.anchored:
+            time_step = float(time_step)
+            self._velocity += self.acceleration * time_step
+            # if any(abs(i) > 0 for i in self._velocity):
+            #     print(self._velocity)
+            self.pos += self._velocity * time_step
+
+    def __str__(self):
+        return "Particle({},{})".format(self.mass, self.pos)
+
+    def __eq__(self, other):
+        return self.mass == other.mass and all(self.pos == other.pos)
+
+
+class ParticleField:
+
+    def __init__(self, particles):
+        self.ps = particles
+
+    def get_particles(self):
+        return self.ps[:]
+
+    def combined(self):
+        avg_mass = sum(self.ps[1:], self.ps[0])
+        return avg_mass
+
+    def time_step(self, step):
+        for index, p in enumerate(self.ps):
+            p.acceleration *= 0
+            for i, j in enumerate(self.ps):
+                if i != index:
+                    p.add_pull(j)
+            p.simulate(step)
+
+
+def test_Particle():
+    p1 = Particle(1, (0, 0))
+    p2 = Particle(2, (1, 1))
+    print(p1 + p2)
+    ps = []
+    for _ in range(10):
+        ps.append(Particle(random.random(), (random.random(), random.random())))
+    print(sum(ps[1:], ps[0]))
+    p1.be_pulled(p2)
+    p1.simulate(1)
+    assert p1.getpos()[0] != 0
+
+
+def test_distance():
+    assert distance(Particle(0, (0, 0)), Particle(0, (0, 1))) == 1
+    assert angle(Particle(0, (0, 0)), Particle(
+        0, (0, 1))) * 180 / math.pi == 90
+
+
+def test_ParticleField():
+    ps = [Particle(random.randint(10, 200), [float(random.randint(0, 1)) for i in range(2)])
+          for i in range(10)]
+    pf = ParticleField(ps)
+    # for i in range(5):
+    #     print([str(i) for i in pf.get_particles()])
+    #     pf.time_step(10)
+    p1i = Particle(10000000000, (0, 0))
+    p1f = Particle(10000000000, (0, 0))
+    p2 = Particle(2, (1, 1))
+    for i in range(100):
+        p1f.be_pulled(p2)
+        p1f.simulate(100000)
+    assert all(p1i.getpos() != p1f.getpos())
+    print(p1f.getpos())
+
+
+def randcolor():
+    return tuple([random.random() for i in range(3)])
+WIDTH, HEIGHT = 1920, 1080
+
+
+def main():
+    # turtle.setup(600, 600)
+    test_Particle()
+    test_distance()
+    test_ParticleField()
+
+    turtle.setup(WIDTH, HEIGHT)
+    # screen.colormode(255)
+    ps = [Particle(random.randint(1e8, 1e12),
+                   [random.randint(-a // 2, a // 2) for a in (WIDTH, HEIGHT)],
+                   velocity=[(random.random() - 0.5) * 8 for i in range(2)]) for i in range(12)]
+    # ps.append(Particle(1e10, [0, 0], anchored=True))
+    ts = [turtle.Turtle() for i in ps]
+    pf = ParticleField(ps)
+    for t, p in zip(ts, ps):
+        # make each turtle unique by color
+        # also set them to max speed
+        # and make them go to the initial positions of their particles.
+        t.pencolor(randcolor())
+        t.speed(0)
+        t.hideturtle()
+        t.goto(p.getpos())
+        t.clear()
+        t.dot((math.log(p.mass)/math.log(10)))
+    granularity = 100
+    time_step = 1
+    try:
+        going = True
+        while going:
+            parts = pf.get_particles()
+            # print(list(map(str, parts)))
+            if all(abs(p.pos[0]) > WIDTH / 2 or abs(p.pos[1]) > HEIGHT / 2 for p in parts):
+                going = False
+            for index, p in enumerate(parts):
+                t = ts[index]
+                # t.clear()
+                # t.penup()
+                pos = p.getpos()
+                t.goto(pos)
+                # t.setx(pos[0])
+                # t.sety(pos[1])
+                # t.dot(math.log(p.mass))
+            # time.sleep(1)
+            for _ in range(granularity):
+                pf.time_step(time_step / granularity)
+        print("DONE")
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print(list(p.velocity for p in ps))
+
+
+if __name__ == '__main__':
+    main()
